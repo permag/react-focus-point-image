0a9d33a177c4820c90de3cf8f56711f93728ea41
Per Magnusson <killingfloor00@gmail.com>
2021-11-01 18:35:58 +0100

    Update example with size and point controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,123 @@
-import FocusPointImage from './lib';
+import { useState } from "react";
 import "./index.css";
 import styled from "styled-components";
+import FocusPointImage from "./lib";
 
-const FlexWrapper = styled.div`
-  display: flex;
+const Container = styled.div`
+  h2, p {
+    margin: 8px 8px 20px;
+  }
+  i {
+    font-size: smaller;
+  }
 `;
 
-const Item = styled.div`
+const ControlSection = styled.section`
   display: flex;
-  flex-direction: column;
-  padding: 20px;
-  flex-shrink: 0;
+  align-items: center;
+  margin: 8px;
+  span {
+    font-size: smaller;
+  }
 `;
 
-const component = (
+const ImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+const getComponent = (point: [number, number]) => (
   <FocusPointImage
     src="parrot.jpg"
     alt="A parrot"
-    focusPoint={[61, 20]}
+    focusPoint={point}
     animate
   />
 );
 
 function App() {
+  const [point, setPoint] = useState<[number, number]>([62, 22.5]);
+  const [size, setSize] = useState<[number, number]>([75, 20])
   return (
-    <>
+    <Container>
       <h2>react-focus-point-image demo</h2>
       <p>
-        Focus is set on the eye of the parrot. Resize the browser window in
-        order to see how the image size changes and the focus at the eye is
-        always kept.
+        Focus is set on one object in an image. When the aspect ratio of the
+        image container changes responsively, the focused part of the image will
+        always be visible.
       </p>
-      <FlexWrapper>
-        <Item>
-          <h3>w: 30vw / h: 40vh</h3>
-          <div style={{ width: "30vw", height: "40vh" }}>{component}</div>
-        </Item>
+      <p>
+        <i>
+          Initially, focus is set on the eye of the parrot. Play around with the
+          container size in order to see how the image changes and how the focus at
+          the eye is always kept.
+        </i>
+      </p>
+      <ControlSection>
+        Set container width:
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={size[0]}
+          step="0.5"
+          onInput={(event: React.FormEvent<HTMLInputElement>) =>
+            setSize([Number(event.currentTarget.value), size[1]])
+          }
+        />
+        <span>{size[0]}</span>
+      </ControlSection>
+      <ControlSection>
+        Set container height:
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={size[1]}
+          step="0.5"
+          onInput={(event: React.FormEvent<HTMLInputElement>) =>
+            setSize([size[0], Number(event.currentTarget.value)])
+          }
+        />
+        <span>{size[1]}</span>
+      </ControlSection>
+      <ControlSection>
+        Set focus point x:
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={point[0]}
+          step="0.5"
+          onInput={(event: React.FormEvent<HTMLInputElement>) =>
+            setPoint([Number(event.currentTarget.value), point[1]])
+          }
+        />
+        <span>{point[0]}</span>
+      </ControlSection>
+      <ControlSection>
+        Set focus point y:
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={point[1]}
+          step="0.5"
+          onInput={(event: React.FormEvent<HTMLInputElement>) =>
+            setPoint([point[0], Number(event.currentTarget.value)])
+          }
+        />
+        <span>{point[1]}</span>
+      </ControlSection>
 
-        <Item>
-          <h3>w: 10vw / h: 40vh</h3>
-          <div style={{ width: "10vw", height: "40vh" }}>{component}</div>
-        </Item>
-      </FlexWrapper>
-
-      <FlexWrapper>
-        <Item>
-          <h3>w: 40vw / h: 10vh</h3>
-          <div style={{ width: "40vw", height: "10vh" }}>{component}</div>
-        </Item>
-
-        <Item>
-          <h3>w: 250px / h: 150px</h3>
-          <div style={{ width: 250, height: 150 }}>{component}</div>
-        </Item>
-      </FlexWrapper>
-    </>
+      <ImageWrapper>
+        <div style={{ width: `${size[0]}vw`, height: `${size[1]}vh` }}>
+          {getComponent(point)}
+        </div>
+      </ImageWrapper>
+    </Container>
   );
 }
 
